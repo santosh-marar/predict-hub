1cbaa7d55d10355f42ed29727acb74ca85c571b9
santosh-marar <st.juniordeveloper@gmail.com>
2025-09-24 00:00:12 +0545

    perf: use pg & add drizzle-studio

--- packages/db/src/database.ts
+++ packages/db/src/database.ts
@@ -4,7 +4,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
+const client = postgres(process.env.DATABASE_URL!, {
+  max: 20, 
+  idle_timeout: 20,
+  connect_timeout: 10, 
+  prepare: false, // Better performance for transactions
+});
 
 export const db = drizzle(client, { schema });
